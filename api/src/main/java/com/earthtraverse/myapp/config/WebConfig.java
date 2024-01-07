288bb55827b0efa80e36ddfb6b14fd398ff6a0af
JarrodMalkovic <jarrodmalkovic@outlook.com>
2024-01-07 20:09:19 +1100

    Update CORS configuration

--- api/src/main/java/com/earthtraverse/myapp/config/WebConfig.java
+++ api/src/main/java/com/earthtraverse/myapp/config/WebConfig.java
@@ -16,10 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins(
-                "http://localhost:4200",
-                "https://earth-traverse.vercel.app"
-        ).allowCredentials(true);
+        registry.addMapping("/**").allowedOrigins("*").allowCredentials(true);
     }
 }
 
