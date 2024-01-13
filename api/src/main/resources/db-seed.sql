72101f8926f4473c2608400d55a0ce018cd19824
JarrodMalkovic <jarrodmalkovic@outlook.com>
2024-01-13 16:50:04 +1100

    Update db seed

--- api/src/main/resources/db-seed.sql
+++ api/src/main/resources/db-seed.sql
@@ -1,16 +1,8 @@
-INSERT INTO map (title, description, image) VALUES
-('Japan', 'Description for Map 1', 'japan.webp');
-INSERT INTO location (country, latitude, longitude, resource_id, map_id) VALUES
-('Japan', 33.96072589999707, 133.50603839999985, '1325381328343686', currval('map_id_seq'),
-('Japan', 33.50845428000008, 135.74962500999982, '4628993663835160', currval('map_id_seq'),
-('Japan', 33.50845428000008, 129.78858384894, '187350169922366', currval('map_id_seq')),
-('Japan', 33.13475440466003, 133.12427554886995, '222649305976165', currval('map_id_seq')),
-('Japan', 34.35550572125236, 142.40858758995148, '622358450049591', currval('map_id_seq')),
-('Japan', 35.859397857049, 140.50351857112, '1059264774713644', currval('map_id_seq')),
-('Japan', 35.0443050280398, 142.92075598361248, '191317532824734', currval('map_id_seq')),
-('Japan', 37.836944444444, 139.0694156, '2334605166731298', currval('map_id_seq')),
-('Japan', 37.990701899999, 140.6292889, '1098747938171888', currval('map_id_seq')),
-('Japan', 32.08012540000004, 130.79814239999973, '838822217419466', currval('map_id_seq'));
+DELETE FROM location;
+DELETE FROM map;
+DELETE from round;
+DELETE from game;
+DELETE FROM answer;
 
 INSERT INTO map (title, description, image) VALUES
 ('China', 'Description for Map 2', 'china.webp');
@@ -97,36 +89,100 @@ INSERT INTO location (country, latitude, longitude, resource_id, map_id) VALUES
 
 INSERT INTO map (title, description, image) VALUES
 ('France', 'Description for Map 9', 'france.webp');
+INSERT INTO location (country, latitude, longitude, resource_id, map_id) VALUES
+('France', 47.614759322580966, 2.0174406717743523, '327212025632899', currval('map_id_seq')),
+('France', 48.0461141, 1.8889661, '261149732955942', currval('map_id_seq')),
+('France', 49.034760200002, 2.6962972, '1546930429183609', currval('map_id_seq')),
+('France', 48.743422, 1.915318, '1008907930513408', currval('map_id_seq')),
+('France', 48.970562475205, 1.889707009316, '231705079872929', currval('map_id_seq'));
 
 INSERT INTO map (title, description, image) VALUES
 ('Germany', 'Description for Map 10', 'germany.webp');
+INSERT INTO location (country, latitude, longitude, resource_id, map_id) VALUES
+('Germany', 50.5174978, 10.7670633, '927274101839505', currval('map_id_seq')),
+('Germany', 50.1131613, 8.7012584, '854617999663639', currval('map_id_seq')),
+('Germany', 51.912470627643, 9.6680290237801, '964862731313040', currval('map_id_seq'));
 
 INSERT INTO map (title, description, image) VALUES
 ('Italy', 'Description for Map 11', 'italy.webp');
+INSERT INTO location (country, latitude, longitude, resource_id, map_id) VALUES
+('Italy', 42.294893345974, 13.537208157593, '164485352722906', currval('map_id_seq')),
+('Italy', 43.375400099999, 13.209693099998, '560791725432374', currval('map_id_seq')),
+('Italy', 43.298956, 10.546975100002, '930141417749567', currval('map_id_seq'));
 
 INSERT INTO map (title, description, image) VALUES
 ('Spain', 'Description for Map 12', 'spain.webp');
+INSERT INTO location (country, latitude, longitude, resource_id, map_id) VALUES
+('Spain', 38.8916888, -3.0255900999987, '768658713814551', currval('map_id_seq')),
+('Spain', 39.779925, -3.9112686000019, '1694534310890196', currval('map_id_seq')),
+('Spain', 40.739138141267, -4.21854834811, '832912941495301', currval('map_id_seq'));
 
 INSERT INTO map (title, description, image) VALUES
 ('Russia', 'Description for Map 13', 'russia.webp');
+INSERT INTO location (country, latitude, longitude, resource_id, map_id) VALUES
+('Russia', 55.359271368995, 39.111541779748, '292779248992342', currval('map_id_seq')),
+('Russia', 56.38697431, 36.73822407, '182016173822263', currval('map_id_seq')),
+('Russia', 54.22851752, 37.37115722, '161804565818247', currval('map_id_seq'));
 
 INSERT INTO map (title, description, image) VALUES
 ('India', 'Description for Map 14', 'india.webp');
+INSERT INTO location (country, latitude, longitude, resource_id, map_id) VALUES
+('India', 23.071038055556, 76.824473055556, '806422976947987', currval('map_id_seq')),
+('India', 24.016205899972, 75.5463136, '550761609984560', currval('map_id_seq')),
+('India', 26.76863685, 79.1018227, '409894470413566', currval('map_id_seq'));
 
 INSERT INTO map (title, description, image) VALUES
 ('Australia', 'Description for Map 15', 'australia.webp');
+INSERT INTO location (country, latitude, longitude, resource_id, map_id) VALUES
+('Australia', -32.320444444444, 116.21191666667, '1637972823066252', currval('map_id_seq')),
+('Australia', -24.371177400002, 132.76891339869, '1637972823066252', currval('map_id_seq')),
+('Australia', -33.222722465966, 149.7455600996, '701300195218063', currval('map_id_seq'));
 
 INSERT INTO map (title, description, image) VALUES
 ('New Zealand', 'Description for Map 16', 'newzealand.webp');
+INSERT INTO location (country, latitude, longitude, resource_id, map_id) VALUES
+('New Zealand', -41.531239722222, 175.48467638889, '316969403187350', currval('map_id_seq')),
+('New Zealand', -45.097591388889, 169.14941388889, '1261805304748350', currval('map_id_seq')),
+('New Zealand', -45.850035, 170.70577, '300456234961410', currval('map_id_seq'));
 
 INSERT INTO map (title, description, image) VALUES
 ('South Africa', 'Description for Map 17', 'southafrica.webp');
+INSERT INTO location (country, latitude, longitude, resource_id, map_id) VALUES
+('South Africa', -31.293487641944, 25.825701551389, '572635543721309', currval('map_id_seq')),
+('South Africa', -34.052955172778, 23.377799754722, '938566686726980', currval('map_id_seq')),
+('South Africa', -34.304991756701, 18.820015153502, '178232877944746', currval('map_id_seq'));
 
 INSERT INTO map (title, description, image) VALUES
 ('Egypt', 'Description for Map 18', 'egypt.webp');
+INSERT INTO location (country, latitude, longitude, resource_id, map_id) VALUES
+('Egypt', 28.202161388889, 28.73586, '760463814834862', currval('map_id_seq')),
+('Egypt', 24.096697, 32.892122, '527701081556756', currval('map_id_seq')),
+('Egypt', 29.322949662773, 31.067067907787, '119488187267234', currval('map_id_seq'));
 
 INSERT INTO map (title, description, image) VALUES
 ('Saudi Arabia', 'Description for Map 19', 'saudiarabia.webp');
+INSERT INTO location (country, latitude, longitude, resource_id, map_id) VALUES
+('Saudia Arabia', 24.7845856, 46.397712699999, '1058168498662997', currval('map_id_seq')),
+('Saudia Arabia', 20.292016016321, 40.114041035997, '765543077446375', currval('map_id_seq')),
+('Saudia Arabia', 24.851194599998, 50.8864732, '1034703617530838', currval('map_id_seq'));
 
 INSERT INTO map (title, description, image) VALUES
 ('Turkey', 'Description for Map 20', 'turkey.webp');
+INSERT INTO location (country, latitude, longitude, resource_id, map_id) VALUES
+('Saudia Arabia', 38.421560369348, 35.122669233368, '709884303911373', currval('map_id_seq')),
+('Saudia Arabia', 40.188142121569, 35.399258425882, '341414337393335', currval('map_id_seq')),
+('Saudia Arabia', 36.707630599996, 33.117100100002, '1795403493953747', currval('map_id_seq'));
+
+INSERT INTO map (title, description, image) VALUES
+('Japan', 'Description for Map 1', 'japan.webp');
+INSERT INTO location (country, latitude, longitude, resource_id, map_id) VALUES
+('Japan', 33.96072589999707, 133.50603839999985, '1325381328343686', currval('map_id_seq')),
+('Japan', 33.50845428000008, 135.74962500999982, '4628993663835160', currval('map_id_seq')),
+('Japan', 33.50845428000008, 129.78858384894, '187350169922366', currval('map_id_seq')),
+('Japan', 33.13475440466003, 133.12427554886995, '222649305976165', currval('map_id_seq')),
+('Japan', 34.35550572125236, 142.40858758995148, '622358450049591', currval('map_id_seq')),
+('Japan', 35.859397857049, 140.50351857112, '1059264774713644', currval('map_id_seq')),
+('Japan', 35.0443050280398, 142.92075598361248, '191317532824734', currval('map_id_seq')),
+('Japan', 37.836944444444, 139.0694156, '2334605166731298', currval('map_id_seq')),
+('Japan', 37.990701899999, 140.6292889, '1098747938171888', currval('map_id_seq')),
+('Japan', 32.08012540000004, 130.79814239999973, '838822217419466', currval('map_id_seq'));
