88665eb352de6d1aa513d11e087e2feb816fe516
JarrodMalkovic <jarrodmalkovic@outlook.com>
2024-01-13 21:53:10 +1100

    Updates

--- client/src/app/game/feature/game.component.ts
+++ client/src/app/game/feature/game.component.ts
@@ -98,11 +98,13 @@ import { SeoService } from 'src/app/shared/utils/seo.service';
     <!-- End screen -->
     <ng-container *ngIf="isOver">
       <div class="flex flex-col h-screen w-screen">
-        <app-map
-          *ngIf="mapMarkers$ | async as mapMarkers"
-          [autofitBounds]="true"
-          [mapMarkers]="mapMarkers"
-        />
+        <div class="flex-grow min-h-0">
+          <app-map
+            *ngIf="mapMarkers$ | async as mapMarkers"
+            [autofitBounds]="true"
+            [mapMarkers]="mapMarkers"
+          />
+        </div>
         <div class="test flex justify-center items-center p-10">
           <div
             class="flex flex-col text-white background-blur-lg bg-white/10 w-full rounded-2xl max-w-7xl shadow-2xl z-[9999] h-fit p-10 space-y-4"
